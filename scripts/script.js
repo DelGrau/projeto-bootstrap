2d2a8e949aae99ad7f6da60cef56d217b517dd4d
Alan Henrique <alanhipermassa@gmail.com>
2022-08-13 00:44:12 -0300

    created npm

---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+$('.content').html("<h3>Hello World</h3>");
+$('.content').prop("$enable-grid-classes: false");
+$('div#frst').addClass('g-col-3 g-start-2');
+$('div#scnd').addClass('g-col-4 g-start-6');
